8f414debe39ce95473bc5eb61acb00d856730db6
k-gnusarev <kgnusaryov@yandex.ru>
2021-06-06 13:15:02 +0400

    fix: github merge problems

---
+++ routes/movies.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { idValidation, movieValidation } = require('../middlewares/requestValidation');
+const {
+  getMovies,
+  createMovie,
+  deleteMovie,
+} = require('../controllers/movies');
+
+router.get('/', getMovies);
+router.post('/', movieValidation, createMovie);
+router.delete('/:movieId', idValidation, deleteMovie);
+
+module.exports = router;
